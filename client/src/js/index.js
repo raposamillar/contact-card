fd6b8247014265b63a15c3fd0dd120fb38323d3f
raposamillar <lisa.raposamillar@gmail.com>
2022-11-03 20:09:57 -0400

    configured and connected the IndexedDB database

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -13,7 +13,10 @@ import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import Bird from '../images/pexels-jonas-11423821.jpg';
 
+import { initdb } from './database';
+
 window.addEventListener('load', function () {
+  initdb();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
